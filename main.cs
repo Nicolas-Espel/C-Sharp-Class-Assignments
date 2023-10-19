fb5d01a27ceb81ddc295d7b70593440933bb0f27
NicoDeGallo8 <13616276-NicoDeGallo8@users.noreply.replit.com>
2023-10-19 16:29:28 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    var personName = "Robert Hall";
+    var birthDate = "2/27/1949";
+    var workPhone = "work 000-444-0101";
+    var cellPhone = "cell 000-444-0189";
+    Console.WriteLine(personName);
+    Console.WriteLine(birthDate);
+    Console.WriteLine(workPhone);
+    Console.WriteLine(cellPhone);
+  }
+}
